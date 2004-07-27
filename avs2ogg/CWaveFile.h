217ae107e302939e1fb7138767c0821f02dd3745
jcsston <>
2004-07-27 02:22:32 +0000

    Fixes for stdout by kassandro

--- avs2ogg/CWaveFile.h
+++ avs2ogg/CWaveFile.h
@@ -6,7 +6,8 @@
 #include <mmsystem.h>
 #include <mmreg.h>
 
-#define SAFE_DELETE(x) if (x != NULL) { delete x; x = NULL; }
+#define SAFE_DELETE(x)			if (x != NULL) { delete x; x = NULL; }
+#define	SAFE_DELETE_ARRAY(x)	if (x != NULL) { delete[] x; x = NULL; }
 #define DXTRACE_ERR(x, ret) ret
 //-----------------------------------------------------------------------------
 // Typing macros
