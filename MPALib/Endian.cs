7d03ec903780358fa8d6d3f90da4d8593817e230
jcsston <>
2004-10-12 23:31:22 +0000

    *** empty log message ***

--- MPALib/Endian.cs
+++ MPALib/Endian.cs
@@ -21,5 +21,15 @@ namespace MPALib
         | ((UInt32)(byte)(buffer[1]) << 16) 
         | ((UInt32)(byte)(buffer[0]) << 24));
     }
+
+    static public UInt64 BuildUInt64From6BytesBE(byte [] buffer) 
+    {
+      return ((UInt64)(byte)(buffer[0]) 
+        | ((UInt64)(byte)(buffer[1]) << 8) 
+        | ((UInt64)(byte)(buffer[2]) << 16) 
+        | ((UInt64)(byte)(buffer[3]) << 24)
+        | ((UInt64)(byte)(buffer[4]) << 32)
+        | ((UInt64)(byte)(buffer[5]) << 40));
+    }
 	}
 }
